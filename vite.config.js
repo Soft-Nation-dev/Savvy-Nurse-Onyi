2aa0789e866e8022b7ee77378f0a65ebc45a293f
soft-nation-dev <emmanuelsoft8105@gmail.com>
2025-06-28 17:00:45 +0100

    fixed error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
+  base: '/soft-nation-dev/', // <--- your repo name
   plugins: [react()],
 })
